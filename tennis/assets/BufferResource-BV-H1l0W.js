e68cc4dfaa7eabaf65f5207b60b09321f57e8d2a
yanchen184 <yanchen184@users.noreply.github.com>
2026-07-23 07:39:48 +0000

    deploy: 57c623574318f74f5b7f5802a59044c52e85509d

--- tennis/assets/BufferResource-BV-H1l0W.js
+++ tennis/assets/BufferResource-BV-H1l0W.js
@@ -1,4 +1,4 @@
-import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-SsqaR4KO.js";const x={name:"local-uniform-bit",vertex:{header:`
+import{w as U,r as g,s as c,u as S,B as _,t as m,E as I,x as p}from"./index-pF8yRmkv.js";const x={name:"local-uniform-bit",vertex:{header:`
 
             struct LocalUniforms {
                 uTransformMatrix:mat3x3<f32>,
